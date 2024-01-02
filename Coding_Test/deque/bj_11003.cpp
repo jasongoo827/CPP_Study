3c1dc0c1d0c374cee66161fe1ef00a6b1e2923fd
jgoo <maruluv0527@gmail.com>
2024-01-02 22:59:32 +0900

    added gitignore

--- Coding_Test/deque/bj_11003.cpp
+++ Coding_Test/deque/bj_11003.cpp
@@ -1,49 +1,49 @@
-#include <iostream>
-#include <queue>
-#define MAX 5000001
+    #include <iostream>
+    #include <queue>
+    #define MAX 5000001
 
-using namespace std;
+    using namespace std;
 
-int n, l;
-int arr[MAX];
+    int n, l;
+    int arr[MAX];
 
-struct Data
-{
-    int idx;
-    int val;
-};
-
-void input()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> n >> l;
-
-    for (int i = 1; i <= n; ++i)
-        cin >> arr[i];
-}
-
-void solve()
-{
-    deque<Data> dq;
-
-    for (int i = 1; i <= n; ++i)
+    struct Data
     {
-        if (!dq.empty() && dq.front().idx == i - l)
-            dq.pop_front();
-        while (!dq.empty() && dq.back().val > arr[i])
-            dq.pop_back();
-        dq.push_back({i, arr[i]});
-        cout << dq.front().val << " ";
+        int idx;
+        int val;
+    };
+
+    void input()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
+        cin >> n >> l;
+
+        for (int i = 1; i <= n; ++i)
+            cin >> arr[i];
     }
-}
 
-int main(void)
-{
-    input();
-    solve();
+    void solve()
+    {
+        deque<Data> dq;
 
-    return (0);
-}
+        for (int i = 1; i <= n; ++i)
+        {
+            if (!dq.empty() && dq.front().idx == i - l)
+                dq.pop_front();
+            while (!dq.empty() && dq.back().val > arr[i])
+                dq.pop_back();
+            dq.push_back({i, arr[i]});
+            cout << dq.front().val << " ";
+        }
+    }
+
+    int main(void)
+    {
+        input();
+        solve();
+
+        return (0);
+    }
