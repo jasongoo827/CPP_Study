d5c10bdb2517d855e40a0a48b23e3d97fc7394a1
jasongoo827 <81475273+jasongoo827@users.noreply.github.com>
2025-08-14 15:29:01 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 196 ms, Memory: 18220 KB -BaekjoonHub

---
+++ Coding_Test/baekjoon/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+int n;
+vector<int> vec;
+vector<int> indices;
+vector<int> lis;
+
+int getLongestPartial()
+{
+    indices.resize(n);
+
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        auto it = lower_bound(lis.begin(), lis.end(), vec[i]);
+
+        int idx = it - lis.begin();
+        if (it == lis.end())
+            lis.push_back(vec[i]);
+        else
+            *it = vec[i];
+        indices[i] = idx;
+    }
+    return static_cast<int>(lis.size());
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s;
+
+    cin >> n;
+    vec.resize(n);
+    for (int i = 0; i < n; ++i)
+        cin >> vec[i];
+    
+    int lisSize = getLongestPartial(); 
+    cout << lisSize << '\n';
+    
+    vector<int> ret;
+    stack<int> st;
+    for (int i = n - 1; i >= 0; --i)
+    {
+        if (indices[i] == lisSize - 1)
+        {
+            lisSize--;
+            st.push(vec[i]);
+        }
+    }
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+}
