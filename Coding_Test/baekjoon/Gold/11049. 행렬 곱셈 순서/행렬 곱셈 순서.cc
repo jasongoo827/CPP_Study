8667102bcd7afe009495d58264501902b84b88b8
jasongoo827 <81475273+jasongoo827@users.noreply.github.com>
2025-08-14 15:22:23 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 32 ms, Memory: 3084 KB -BaekjoonHub

---
+++ Coding_Test/baekjoon/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+// boj 11049
+
+int n;
+int row[500];
+int col[500];
+
+int cost(int i, int k, int j)
+{
+    return row[i] * col[k] * col[j];
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+    vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+    
+    for (int i = 0; i < n; ++i)
+        cin >> row[i] >> col[i];
+
+    for (int i = 0; i < n; ++i)
+        dp[i][i] = 0;
+    
+    for (int len = 2; len <= n; ++len)
+    {
+        for (int i = 0; i < n - len + 1; ++i)
+        {
+            int j = i + len - 1;
+            for (int k = i; k < j; ++k)
+            {
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + cost(i, k, j));
+            }
+        }
+    }
+
+    cout << dp[0][n - 1] << '\n';
+
+    return 0;
+}
