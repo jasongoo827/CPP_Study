2f1c3486e789b9df1abf6425178b57f6eb29f982
jasongoo827 <81475273+jasongoo827@users.noreply.github.com>
2025-08-14 15:32:40 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ Coding_Test/baekjoon/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+
+void dfs(vector<int>& v, vector<int>& ret, int picked, vector<bool>& used)
+{
+    if (picked == m)
+    {
+        for (int num: ret)
+            cout << num << " ";
+        cout << '\n';
+        return;
+    }
+    
+    int prev = -1;
+    for (int i = 0; i < n; ++i)
+    {
+        if (used[i])
+            continue;
+        if (v[i] == prev)
+            continue;
+        if (picked > 0 && v[i] < ret[picked - 1])
+            continue;
+        
+        ret[picked] = v[i];
+        used[i] = true;
+        dfs(v, ret, picked + 1, used);
+        used[i] = false;
+        prev = v[i];
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> n >> m;
+    
+    vector<int> v(n);
+    vector<bool> used(n, false);
+    
+    for (int i = 0; i < n; ++i)
+        cin >> v[i];
+    sort(v.begin(), v.end());
+    
+    vector<int> ret(m);
+    dfs(v, ret, 0, used);
+    
+    return 0;
+}
