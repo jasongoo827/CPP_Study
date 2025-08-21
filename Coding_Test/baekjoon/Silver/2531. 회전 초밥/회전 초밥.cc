cb1fddc0fedecdff3e4abd59f133371e37ca9d01
jasongoo827 <81475273+jasongoo827@users.noreply.github.com>
2025-08-21 13:40:53 +0900

    [Silver I] Title: 회전 초밥, Time: 0 ms, Memory: 2256 KB -BaekjoonHub

--- Coding_Test/baekjoon/Silver/2531. 회전 초밥/회전 초밥.cc
+++ Coding_Test/baekjoon/Silver/2531. 회전 초밥/회전 초밥.cc
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-
 using namespace std;
 
 int main()
@@ -16,29 +14,27 @@ int main()
     for (int i = 0; i < n; ++i)
         cin >> v[i];
     
-    unordered_map<int, int> umap;
+    int sushi[3001] = {0};
+    int variety = 0;
     int ret = 0;
-    // 7 9 7 30 2 7 9 25
-    for (int i = 0; i < k; ++i)
-        umap[v[i]]++;
     
-    if (!umap.count(c))
-        ret = max(ret, (int)umap.size() + 1);
-    else
-        ret = max(ret, (int)umap.size());
-    
-    for (int i = 1; i < n; ++i)
-    {
-        umap[v[i - 1]]--;
-        if (umap[v[i - 1]] == 0)
-            umap.erase(v[i - 1]);
-        umap[v[(i + k - 1) % n]]++;
-        
-        if (!umap.count(c))
-            ret = max(ret, (int)umap.size() + 1);
-        else
-            ret = max(ret, (int)umap.size());
+    for (int i = 0; i < k; ++i) {
+        if (sushi[v[i]]++ == 0) 
+            variety++;
     }
+    
+    ret = variety + (sushi[c] == 0 ? 1 : 0);
+    
+    for (int i = 1; i < n; ++i) {
+        if (--sushi[v[i - 1]] == 0)
+            variety--;
+        
+        if (sushi[v[(i + k - 1) % n]]++ == 0)
+            variety++;
+        
+        ret = max(ret, variety + (sushi[c] == 0 ? 1 : 0));
+    }
+    
     cout << ret << '\n';
     
     return 0;
